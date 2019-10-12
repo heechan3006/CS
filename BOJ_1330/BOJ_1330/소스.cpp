9c03243c44d5ba4c66e4d71ca2b9e42ce51830d2
heechanYang <heechan3006@gmail.com>
2019-10-12 15:46:32 +0900

    테스트

---
+++ BOJ_1330/BOJ_1330/소스.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	if (n < m) cout << "<";
+	else if (n > m) cout << ">";
+	else cout << "==";
+	return 0;
+}
